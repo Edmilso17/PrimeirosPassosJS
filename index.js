79a51912b103c00d8ffcaada0d481e817a8f839b
edmilson17 <edmilsonribeiro.17072005@gmail.com>
2022-11-11 21:00:11 -0300

    feito

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let nome = prompt ("Digite seu primeiro nome:")
-let sobrenome = prompt ("Digite seu segundo nome:")
+let nome = prompt ("Digite seu primeiro nome:");
+let sobrenome = prompt ("Digite seu segundo nome:");
 
 alert(`Seu nome é: ${nome} ${sobrenome}`)
+alert(`Seu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`)
